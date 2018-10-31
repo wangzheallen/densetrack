cc4ac207bbc0c8fd66ef91b80d2a8a080e8e9b5f
Andreas Girgensohn <andreasg123@users.noreply.github.com>
2018-10-31 11:51:29 -0700

    Proper paths for conda in setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,51 @@
 #!/usr/bin/env python
 
+import os
 from setuptools.command.build_ext import build_ext
 from distutils.core import setup, Extension
 
 USE_SURF = True
 
 
-# Avoid a gcc warning below: https://stackoverflow.com/a/49041815
-# cc1plus: warning: command line option ‘-Wstrict-prototypes’ is valid
-# for C/ObjC but not for C++
-class BuildExt(build_ext):
+class CustomBuildExt(build_ext):
     def build_extensions(self):
+        # Avoid a gcc warning: https://stackoverflow.com/a/49041815
+        # cc1plus: warning: command line option ‘-Wstrict-prototypes’ is valid
+        # for C/ObjC but not for C++
         self.compiler.compiler_so.remove('-Wstrict-prototypes')
-        super(BuildExt, self).build_extensions()
+        super(CustomBuildExt, self).build_extensions()
+    def run(self):
+        # import numpy after install_requires
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+        build_ext.run(self)
 
+
+try:
+    include_dirs = [os.path.join(os.environ['CONDA_PREFIX'], 'include')]
+    library_dirs = [os.path.join(os.environ['CONDA_PREFIX'], 'lib')]
+except KeyError:
+    include_dirs = []
+    library_dirs = []
 
 extra_compile_args = ['-O3']
 macros = [('USE_PYTHON', None)]
 libs = ['opencv_videoio', 'opencv_calib3d', 'opencv_features2d',
         'opencv_highgui', 'opencv_imgproc', 'opencv_imgcodecs', 'opencv_core']
-#        'avformat', 'avdevice', 'avutil', 'avcodec', 'swscale']
 if USE_SURF:
     macros.append(('USE_SURF', None))
     libs.append('opencv_xfeatures2d')
 
 densetrack = Extension('densetrack', ['src/DenseTrackStab.cpp'],
+                       include_dirs=include_dirs,
+                       library_dirs=library_dirs,
                        extra_compile_args=extra_compile_args,
                        libraries=libs, define_macros=macros)
 
 setup(name='densetrack',
       version='1.0',
-      cmdclass={'build_ext': BuildExt},
+      cmdclass={'build_ext': CustomBuildExt},
+      install_requires=['numpy'],
       ext_modules=[densetrack],
       url='https://github.com/FXPAL/densetrack',
       author='Andreas Girgensohn',
